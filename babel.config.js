d2e00200efb8a87d2c41eb64d3aba90662c1ccf4
大鲨鱼 <164063721@qq.com>
2026-03-30 19:52:00 +0800

    Simplify babel config - remove module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '@': './src',
-          '@components': './src/components',
-          '@screens': './src/screens',
-          '@navigation': './src/navigation',
-          '@store': './src/store',
-          '@utils': './src/utils',
-          '@assets': './src/assets',
-        },
-      },
-    ],
+    'react-native-reanimated/plugin',
   ],
 };
